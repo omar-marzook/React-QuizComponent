74c7253b5f83c94ee95109fa7b5714791a750cc0
omar-marzook <omarmarzook92@gmail.com>
2019-11-11 15:15:56 +0200

    Displaying An Error Message Based on State

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,9 +10,7 @@ class Quiz extends Component {
   }
 
   showNextQuestion() {
-    this.setState(state => {
-      return { quiz_position: state.quiz_position + 1 };
-    });
+    this.setState({ quiz_position: this.state.quiz_position + 1 });
   }
 
   render() {
